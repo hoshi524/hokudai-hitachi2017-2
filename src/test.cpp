736f055d66da36d0ae105173df57a4d1453857ba
hoshi524 <hoshi524@gmail.com>
2017-12-04 12:48:21 +0900

    update

--- src/test.cpp
+++ src/test.cpp
@@ -59,26 +59,16 @@ inline unsigned get_random() {
   return y ^= (y ^= (y ^= y << 13) >> 17) << 5;
 }
 
-constexpr int ROW = 1 << 5;
+constexpr int ROW = 1 << 6;
 constexpr int MAX_V = 1 << 9;
 constexpr int MAX_KV = ROW * ROW;
-constexpr int WALL = MAX_V - 1;
 
 int V, E, KV, KE, KR;
-uint8_t W[MAX_V][MAX_V];
-uint8_t P[MAX_KV];
-uint16_t X[MAX_KV];
-
-int bestScore = 0;
-uint16_t best[MAX_KV];
-
-struct Node {
-  int16_t pos;
-  int16_t vertex;
-  int16_t score;
-  int value;
+int W[MAX_V][MAX_V];
+int X[MAX_KV];
+constexpr static int direction[] = {
+    -ROW - 1, -ROW, -ROW + 1, -1, +1, +ROW - 1, +ROW, +ROW + 1,
 };
-Node state[MAX_KV][MAX_V];
 
 int main() {
   {  // input
@@ -95,118 +85,66 @@ int main() {
     scanf("%d%d\n", &KV, &KE);
     KR = sqrt(KV);
   }
-  int R = min(KR, ROW - 2);
   {  // Hill Climbing
-    Node* bestVertex[MAX_KV];
-    bool ok[MAX_KV];
-    memset(ok, false, sizeof(ok));
-    vector<int16_t> vertexes(MAX_V);
-    vertexes.clear();
-    vector<int16_t> positions(MAX_KV);
-    positions.clear();
-    for (int i = 0; i < MAX_KV; ++i) X[i] = WALL;
-    for (int i = 0; i < V; ++i) vertexes.emplace_back(i);
-    for (int i = 1; i <= R; ++i) {
-      for (int j = 1; j <= R; ++j) {
-        int p = i * ROW + j;
-        ok[p] = true;
-        X[p] = V;
-        bestVertex[p] = &state[p][vertexes[0]];
-        for (int k = 0; k < V; ++k) {
-          state[p][k].pos = p;
-          state[p][k].vertex = k;
-          state[p][k].score = 0;
-          state[p][k].value = INT_MIN;
+  start:
+    for (int i = 0; i < MAX_KV; ++i) X[i] = V;
+    vector<int> RemainVertex;
+    for (int i = 0; i < V; ++i) RemainVertex.push_back(i);
+    vector<int> Position;
+    Position.push_back(KR / 2 * ROW + KR / 2);
+    vector<int> used;
+    for (int i = 0; i < V; ++i) {
+      static int distance[MAX_KV][MAX_KV];
+      memset(distance, 0x0f, sizeof(distance));
+      for (int j = 0; j < V; ++j) distance[j][j] = 0;
+      for (int p : Position) {
+        for (int d : direction) {
+          int n = p + d;
+          if (X[n] < V) continue;
+          distance[p][n] = 1;
+          distance[n][p] = 1;
         }
       }
-    }
-    int score = 0;
-    auto update = [&](int n, int p) {
-      if (X[p] != V) return;
-      int value = -1;
-      auto add = [&](Node& n, int v, int u) {
-        if (u < V) {
-          int t = W[v][u];
-          if (t) {
-            n.score += t;
-            n.value += t << 14;
-          } else {
-            n.value -= 1 << 10;
+      for (int k : Position) {
+        if (X[k] < V) continue;
+        for (int i : Position) {
+          for (int j : Position) {
+            int d = distance[i][k] + distance[k][j];
+            if (distance[i][j] > d) distance[i][j] = d;
           }
         }
-      };
-      for (int v : vertexes) {
-        if (ok[p]) {
-          state[p][v].score = 0;
-          state[p][v].value = 0;
-          add(state[p][v], v, X[p - ROW - 1]);
-          add(state[p][v], v, X[p - ROW]);
-          add(state[p][v], v, X[p - ROW + 1]);
-          add(state[p][v], v, X[p - 1]);
-          add(state[p][v], v, X[p + 1]);
-          add(state[p][v], v, X[p + ROW - 1]);
-          add(state[p][v], v, X[p + ROW]);
-          add(state[p][v], v, X[p + ROW + 1]);
-        } else {
-          add(state[p][v], v, X[n]);
-        }
-        int t = state[p][v].value + (get_random() & ((1 << 10) - 1));
-        if (value < t) {
-          value = t;
-          bestVertex[p] = &state[p][v];
-        }
       }
-      if (ok[p]) {
-        ok[p] = false;
-        positions.emplace_back(p);
-      }
-    };
-    update(-1, (R / 2 + 1) * ROW + (R / 2 + 1));
-    for (int i = 0; i < V; ++i) {
-      Node* n = bestVertex[positions[0]];
-      int value = INT_MIN;
-      for (int p : positions) {
-        if (bestVertex[p]->value < 0) {
-          int value = INT_MIN;
-          for (int v : vertexes) {
-            if (value < state[p][v].value) {
-              value = state[p][v].value;
-              bestVertex[p] = &state[p][v];
-            }
+      int vertex = -1;
+      int value = 0;
+      for (int v : RemainVertex) {
+        static vector<int> target;
+        static set<int> targetVertex;
+        target.clear();
+        targetVertex.clear();
+        for (int p : used) {
+          if (W[v][X[p]]) {
+            target.push_back(p);
+            targetVertex.insert(X[p]);
           }
         }
-        int t = bestVertex[p]->value + (get_random() & ((1 << 10) - 1));
-        if (value < t) {
-          value = t;
-          n = bestVertex[p];
+        if (target.empty()) continue;
+        for (int n : target) {
+          static int VertexDistance[MAX_V];
+          memset(VertexDistance, 0x0f, sizeof(VertexDistance));
+          for (int m : target) {
+            int d = distance[n][m];
+            if (VertexDistance[X[m]] > d) VertexDistance[X[m]] = d;
+          }
+          int v = 0;
+          for (int n : targetVertex)
+            if (VertexDistance[n] < 0xff) v += 0xffff - VertexDistance[n];
+          if (value < v) {
+            value = v;
+            vertex = v;
+          }
         }
       }
-      X[n->pos] = n->vertex;
-      vertexes.erase(find(vertexes.begin(), vertexes.end(), n->vertex));
-      positions.erase(find(positions.begin(), positions.end(), n->pos));
-      score += n->score;
-      for (int p : positions) state[p][n->vertex].value = -1000000;
-      update(n->pos, n->pos - ROW - 1);
-      update(n->pos, n->pos - ROW);
-      update(n->pos, n->pos - ROW + 1);
-      update(n->pos, n->pos - 1);
-      update(n->pos, n->pos + 1);
-      update(n->pos, n->pos + ROW - 1);
-      update(n->pos, n->pos + ROW);
-      update(n->pos, n->pos + ROW + 1);
-    }
-    if (bestScore < score) {
-      bestScore = score;
-      memcpy(best, X, sizeof(X));
-    }
-  }
-  {  // output
-    int P[MAX_V];
-    for (int i = 0; i < MAX_KV; ++i) {
-      if (best[i] < V) P[best[i]] = (i / ROW - 1) * KR + i % ROW;
-    }
-    for (int i = 0; i < V; ++i) {
-      printf("1 %d\n", P[i]);
+      if (vertex == -1) goto start;
     }
   }
 }
